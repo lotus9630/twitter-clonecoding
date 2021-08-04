c36c798dd42f3d27c2eced5a1b2472b998dfd6e3
lotus9630 <68753108+lotus9630@users.noreply.github.com>
2021-08-05 08:49:02 +0900

    3

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,24 @@ import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   const [init, setInit] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+  console.log(userObj);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
-      setIsLoggedIn(user ? true : false);
+      if (user) {
+        setUserObj(user);
+      }
       setInit(true);
     });
   }, []);
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      {init ? (
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+      ) : (
+        "Initializing..."
+      )}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -11,5 +13,7 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-export const authService = firebase.auth();
+
 export const firebaseInstance = firebase;
+export const authService = firebase.auth();
+export const dbService = firebase.firestore();
